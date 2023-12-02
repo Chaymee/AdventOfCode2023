457b686d24f9ffe0f76b335edfb009757e25c4bb
Jamieson Walker <fr.golgo@gmail.com>
2023-12-01 21:06:57 -0600

    added part2

--- challenges/day1/day1.py
+++ challenges/day1/day1.py
@@ -1,6 +1,6 @@
 import sys
 
-INTS = ["one", "two", "three", "four", "five", "six", "seven", "eight","nine"]
+INTS = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8","nine":"9"}
 
 def main():
     mode = sys.argv[1]
@@ -15,15 +15,18 @@ def main():
     lines = input.readlines()
     total = 0
     for line in lines:
-        numbers = []
-        line.casefold()
-        print(line.index("two"))
-        for value in line:
-            if value.isnumeric():
-                numbers.append(value)
-        result = numbers[0] + numbers.pop()
-        total += int(result)
+     total += fix_numbers(line)
     print(total)
+
+def fix_numbers(s: str) -> int:
+    numbers = []
+    for num, digit in INTS.items():
+        s = s.replace(num, f"{num[0]}{digit}{num[-1]}")
+    for value in s:
+        if value.isnumeric():
+            numbers.append(value)
+    result = numbers[0] + numbers.pop()
+    return int(result)
 
 if __name__ == "__main__":
     main()
